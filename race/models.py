9e82b3e9dd348265bfaae055651e4efa383d428d
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-26 10:43:39 -0300

    feat: cria app race e modelo de banco de dados

---
+++ race/models.py
@@ -0,0 +1,10 @@
+# race/models.py
+from django.db import models
+
+class RegistroDeVolta(models.Model):
+    veiculo = models.CharField(max_length=50)
+    tempo_segundos = models.FloatField()
+    data_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.veiculo} - {self.tempo_segundos}s"
